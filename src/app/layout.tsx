07d619991d7febe56c4eac014a9d8633a46f4fdd
nicopache97 <nicopacheco1997@gmail.com>
2026-03-13 20:30:12 -0300

    chore: Update project name and version, and remove `react-grab` and `same-runtime` dependencies and related configurations.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,14 +23,7 @@ export default function RootLayout({
     return (
         <html lang="en" className={inter.variable}>
             <head>
-                <Script
-                    crossOrigin="anonymous"
-                    src="//unpkg.com/react-grab/dist/index.global.js"
-                />
-                <Script
-                    crossOrigin="anonymous"
-                    src="//unpkg.com/same-runtime/dist/index.global.js"
-                />
+
             </head>
             <body suppressHydrationWarning className="antialiased">
                 <ClientBody>{children}</ClientBody>
